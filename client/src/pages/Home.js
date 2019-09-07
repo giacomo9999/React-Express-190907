0bf4b9a9cdef134863d86349549c49c6c3ca94b5
James Gary <gmarchesi888@gmail.com>
2019-09-07 12:54:50 -0400

    Simple routing working.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
-    render(){
-        return (
-            <h1>I Am The Home</h1>
-        )
-    }
+  render() {
+    return (
+      <div className="container-outer">
+        <h1>I Am The Home</h1>
+        <Link to={"./list"}>
+          <button>My List</button>
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default Home;
